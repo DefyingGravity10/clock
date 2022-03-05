986a32bad5a415b53a12cf9ea8f1ec1eb221c922
DefyingGravity10 <64212136+DefyingGravity10@users.noreply.github.com>
2022-03-05 21:18:00 +0800

    Ensured that parameters yielding values less than 10 will be prepended by a "0"

--- clock.js
+++ clock.js
@@ -6,6 +6,14 @@ function getTime() {
 
     let time = document.querySelector(".clock");
 
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
     if (seconds < 10) {
         seconds = "0" + seconds;
     }
@@ -13,7 +21,6 @@ function getTime() {
     let currentTime = hours + ":" + minutes + ":" + seconds;
     time.textContent = currentTime;
 }
-
 
 getTime();
 setInterval(getTime, 1000);
