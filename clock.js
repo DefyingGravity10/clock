1d7963f220952c7db95d769849a52d8b1aa220f3
DefyingGravity10 <64212136+DefyingGravity10@users.noreply.github.com>
2022-03-06 13:34:42 +0800

    Added feature wherein the clock also displays the current date (MM/DD/YYYY format)

--- clock.js
+++ clock.js
@@ -1,10 +1,14 @@
-function getTime() {
+function getTimeNDate() {
     let now = new Date();
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
+    let year = now.getFullYear();
+    let month = now.getMonth();
+    let day = now.getDate();
 
-    let time = document.querySelector(".clock");
+    let time = document.querySelector(".time");
+    let date = document.querySelector(".date");
 
     if (hours < 10) {
         hours = "0" + hours;
@@ -20,7 +24,10 @@ function getTime() {
 
     let currentTime = hours + ":" + minutes + ":" + seconds;
     time.textContent = currentTime;
+
+    let currentDate = month + "/" + day + "/" + year;
+    date.textContent = currentDate;
 }
 
-getTime();
-setInterval(getTime, 1000);
+getTimeNDate();
+setInterval(getTimeNDate, 1000);
